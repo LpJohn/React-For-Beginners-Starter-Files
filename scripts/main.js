f3d6a35afbe941050355a6345b96cf22977552f3
John Hilts <johnhilts@live.com>
2016-03-24 17:10:01 -0700

    added history push but it's an obsolete library so doesn't work

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,8 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
+var Navigation = ReactRouter.Navigation; // mixin
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers');
 
@@ -72,11 +73,19 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
+	mixins : [History],
+
+	goToStore : function(event) {
+		event.preventDefault();
+		// get the data from the input
+		var storeId = this.refs.storeId.value;
+		this.history.pushState(null, '/store/' + storeId);
+	},
 
 	render : function() {
 		var name = "John";
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please enter a store</h2>
 				{/* this is a comment - where does it show up? */}
 				<input type="text" ref="storeId" defaultValue={h.getFunName()} required />

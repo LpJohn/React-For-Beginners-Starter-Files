93472156e14acac605c3c65fd658980f1537ea53
John Hilts <johnhilts@live.com>
2016-03-27 17:59:47 -0700

    added missing key that React needs for enumerating, etc - Video 16

--- scripts/main.js
+++ scripts/main.js
@@ -171,7 +171,7 @@ var Order = React.createClass({
 		}
 
 		return (
-			<li>
+			<li key={key}>
 				<span>{count}</span>lbs
 				{fish.name}
 				<span className="price">{h.formatPrice(count * fish.price)}</span>

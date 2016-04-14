9f382948038ed25e7bac24cb81799ddcac1e516a
John Hilts <johnhilts@live.com>
2016-04-13 22:14:49 -0700

    chunked out components into ES6 modules Part 4 Video #22

--- scripts/main.js
+++ scripts/main.js
@@ -1,123 +1,17 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var h = require('./helpers');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
 
 import { browserHistory } from 'react-router'
 
-// Firebase
-var Rebase = require('re-base');
-var base = Rebase.createClass('https://catch-of-the-day-lpj.firebaseio.com/');
-
-var Catalyst = require('react-catalyst');
 
 /*
 	Import Components
 */
 
-import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
-import Header from './components/Header';
-import Fish from './components/Fish';
-import Order from './components/Order';
-import Inventory from './components/Inventory';
-
-/*
-  App
-*/
-
-var App = React.createClass({
-	
-	mixins : [Catalyst.LinkedStateMixin],
-
-	getInitialState : function() {
-		return {
-			fishes : {},
-			order : {},
-		}
-	},
-
-	// NOTE: use this to initialize with server-side info
-	// also, you can get more info from react docs
-	// https://facebook.github.io/react/docs/component-specs.html
-	componentDidMount : function() {
-		base.syncState(this.props.params.storeId + '/fishes', {
-		context : this,
-		state : 'fishes'
-		});
-
-		var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
-
-		if (localStorageRef) {
-			// update our component state to reflect what is in localStorage
-			this.setState({
-				order : JSON.parse(localStorageRef)
-			});
-		}
-	},
-
-	componentWillUpdate : function(nextProps, nextState) {
-		localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
-	},
-
-	addToOrder : function(key) {
-		this.state.order[key] = this.state.order[key] + 1 || 1;
-		this.setState({order : this.state.order });
-	},
-
-	removeFromOrder : function(key) {
-		delete this.state.order[key];
-		this.setState({
-			order : this.state.order
-		});
-	},
-
-	addFish : function(fish) {
-		var timestamp = (new Date()).getTime();
-		// update the state object
-		this.state.fishes['fish-' + timestamp] = fish;
-		// set the state
-		this.setState({ fishes : this.state.fishes });
-	},
-
-	removeFish : function(key) {
-		if (confirm("Are you sure you want to remove this fish?!")) {
-			this.state.fishes[key] = null;
-			this.setState({
-				fishes : this.state.fishes
-			});
-		}
-	},
-
-	loadSamples : function() {
-		this.setState({
-			fishes : require('./sample-fishes')
-		});
-	},
-
-
-	renderFish : function(key) {
-		return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
-	},
-
-  render : function() {
-    return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header tagline="Fresh Seafood Good"/>
-	  <ul className="list-of-fishes">
-	 	{Object.keys(this.state.fishes).map(this.renderFish)}
-	  </ul>
-        </div>  
-        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish} />
-      </div>
-    )
-  }
-});
+import NotFound from './components/NotFound';
+import App from './components/App';
 
 /*
 	Routes

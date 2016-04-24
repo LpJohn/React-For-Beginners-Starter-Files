453b7dec2aa26011d8d5b2fe8f5769f1f9bb29dc
John Hilts <johnhilts@live.com>
2016-04-23 23:24:34 -0700

    Added authentication Video #27 this still doesn't load in the state very well after auth ... not sure if it's me or the code from the video

--- scripts/components/Inventory.js
+++ scripts/components/Inventory.js
@@ -9,8 +9,82 @@ import autobind from 'autobind-decorator';
 
 import AddFishForm from './AddFishForm';
 
+import Firebase from 'firebase';
+const ref = new Firebase('https://catch-of-the-day-lpj.firebaseio.com/');
+
 @autobind
 class Inventory extends React.Component{
+
+	constructor() {
+		super();
+
+		this.state = {
+			uid: ''
+		}
+	}
+
+	authenticate(provider) {
+		console.log("Trying to auth with " + provider);
+		ref.authWithOAuthPopup(provider, this.authHandler);
+	}
+
+	componentWillMount() {
+		console.log("Checking to see if we can log them in");
+		var token = localStorage.getItem('token');
+		if (token) {
+			ref.authWithCustomToken(token, this.authHandler);
+		}
+	}
+
+	logout() {
+		ref.unauth();
+		localStorage.removeItem('token');
+		this.setState({
+			uid : null
+		});
+	}
+
+	authHandler(err, authData) {
+		if(err) {
+			console.error(err);
+			return;
+		}
+
+
+		// save the login token in the browser
+		localStorage.setItem('token', authData.token);
+
+		console.log(this.props.params.storeId);
+		const storeRef = ref.child(this.props.params.storeId);
+		storeRef.on('value', (snapshot) => {
+			var data = snapshot.val()
+			});
+
+		if(!authData.owner) {
+			// claim it as our own
+			storeRef.set({
+				owner : authData.uid
+			});
+		}
+
+		// update our state to reflect the current store owner and user
+		this.setState({
+			uid : authData.uid,
+			owner : authData.owner || authData.uid
+			});
+	}
+
+	renderLogin() {
+		return (
+			<nav className="login">
+				<h2>Inventory</h2>
+				<p>Sign in to manage your store's inventory</p>
+				<p>put email and password here</p>
+				<button onClick={this.authenticate.bind(this, 'github')} >Fake Login</button>
+			</nav>
+		)
+
+	}
 
 	renderInventory(key) {
 		var linkState = this.props.linkState;
@@ -28,10 +102,32 @@ class Inventory extends React.Component{
 			</div>
 		)
 	}
+
   render() {
+
+  	let logoutButton = <button onClick={this.logout}>Log Out!</button>
+
+  	// first check if they aren't logged in
+  	if (!this.state.uid) {
+		return (
+			<div>{this.renderLogin()}</div>
+		       )
+	}
+
+	// then check if they aren't the owner of the current store
+	if(this.state.uid !== this.state.owner) {
+		return (
+			<div>
+				<p>Sorry, you aren't the owner of this store</p>
+				{logoutButton}
+			</div>
+		       )
+	}
+
     return (
       <div>
 	      <h2>Inventory</h2>
+	      {logoutButton}
 	      {Object.keys(this.props.fishes).map(this.renderInventory)}
 
 	      <AddFishForm {...this.props} />
